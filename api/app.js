95727e7d291533da9a77fb1cd1ed53b45bc56b31
Sam Atkins <samatkins5@gmail.com>
2018-03-08 21:33:47 +0000

    feat: Add simple Authorization header handler

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('./handlers/logger');
+const auth = require('./handlers/auth');
 const routes = require('./routes/index');
 
 const app = express();
 
 app.use(logger.devLogger);
+
+app.use(auth.authHeadersHandler);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ api/handlers/auth.js
@@ -0,0 +1,12 @@
+exports.authHeadersHandler = (req, res, next) => {
+  const token = req.get('Authorization');
+
+  if (token) {
+    req.token = token;
+    next();
+  } else {
+    res.status(403).send({
+      error: 'Please provide an Authorization header to identify yourself',
+    });
+  }
+};
