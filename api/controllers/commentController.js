d8fab7dbcc01ffe895a2962f03087e5c18127a6c
Sam Atkins <samatkins5@gmail.com>
2018-03-12 20:33:45 +0000

    refactor: Reorder functions

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -11,41 +11,11 @@ exports.addComment = async (req, res) => {
   }
 };
 
-exports.getComments = async (req, res) => {
+exports.deleteComment = async (req, res) => {
   try {
-    const comments = await Comment.find({ parentId: req.params.id }).exec();
-    res.status(200).json(comments);
-  } catch (error) {
-    res.status(400).send('Bad Request');
-  }
-};
-
-exports.voteUpComment = async (req, res) => {
-  try {
-    const comment = await Comment.findOneAndUpdate(
-      { _id: req.params.id },
-      { $inc: { voteScore: +1 } },
-      {
-        new: true,
-        runValidators: true,
-      }
-    ).exec();
-    res.status(200).json(comment);
-  } catch (error) {
-    res.status(400).send('Bad Request');
-  }
-};
-
-exports.voteDownComment = async (req, res) => {
-  try {
-    const comment = await Comment.findOneAndUpdate(
-      { _id: req.params.id },
-      { $inc: { voteScore: -1 } },
-      {
-        new: true,
-        runValidators: true,
-      }
-    ).exec();
+    const comment = await Comment.findOneAndRemove({
+      _id: req.params.id,
+    }).exec();
     res.status(200).json(comment);
   } catch (error) {
     res.status(400).send('Bad Request');
@@ -68,11 +38,41 @@ exports.editComment = async (req, res) => {
   }
 };
 
-exports.deleteComment = async (req, res) => {
+exports.getComments = async (req, res) => {
   try {
-    const comment = await Comment.findOneAndRemove({
-      _id: req.params.id,
-    }).exec();
+    const comments = await Comment.find({ parentId: req.params.id }).exec();
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(400).send('Bad Request');
+  }
+};
+
+exports.voteDownComment = async (req, res) => {
+  try {
+    const comment = await Comment.findOneAndUpdate(
+      { _id: req.params.id },
+      { $inc: { voteScore: -1 } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    ).exec();
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(400).send('Bad Request');
+  }
+};
+
+exports.voteUpComment = async (req, res) => {
+  try {
+    const comment = await Comment.findOneAndUpdate(
+      { _id: req.params.id },
+      { $inc: { voteScore: +1 } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    ).exec();
     res.status(200).json(comment);
   } catch (error) {
     res.status(400).send('Bad Request');
